6ef51d4cc428b212e564d6ab3a8e00fede9ae20c
Vadimdimdim <vadim.egorov1304@gmail.com>
2020-06-23 22:55:51 -0800

    move private iformation into dev.js

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
-mongoose.connect('mongodb+srv://VadimEgorov:angel1972@cluster0-9xth0.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+const config = require("./config/key");
+
+const {User} = require("./model/user");
+
+mongoose.connect(config.mongoURI, 
     {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('DB CONNECTED'))
     .catch(err => console.error(err));
 
-app.get("/", function(req, res){
-    res.send("test");
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.json({"hello ~": "Hi ~~ asdaasdasd"});
+});
+
+app.post("/api/users/register", (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, userData) => {
+        if(err) return res.json ({success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+    });
 });
 
 app.listen("5000", function(){
